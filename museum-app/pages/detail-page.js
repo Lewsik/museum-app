7fdee9835807936ae28513787330694c8e6f6edd
Lucia D'Ippolito <lbdlucibell@gmail.com>
2019-03-20 21:34:21 +0100

    Added function doesNotPassAllValidations

--- museum-app/pages/detail-page.js
+++ museum-app/pages/detail-page.js
@@ -1,9 +1,23 @@
+function doesNotPassAllValidations(name, msg) {
+    if (!name || !msg) {
+        alert('You forgot to fill in your name or message!')
+        return true
+    }
+
+    if (msg.length > 280) {
+        alert('Your comment is too long')
+        return true
+    }
+
+    return false
+}
+
 function submitComment() {
     const inputField = document.getElementById('name')
     const name = inputField.value
     const textArea = document.getElementById('msg')
     const msg = textArea.value
-    
+
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
@@ -19,4 +33,8 @@ function submitComment() {
 
     inputField.value = null
     textArea.value = null
+
+    if (doesNotPassAllValidations(name, msg)) {
+        return null
+    }
 }
